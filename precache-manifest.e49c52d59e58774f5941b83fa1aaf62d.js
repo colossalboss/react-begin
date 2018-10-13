660146400306495cd57f7842f3171b1a6409eb34
Ogbara G. Godstar <colossalboss@users.noreply.github.com>
2018-10-13 22:08:49 +0100

    Updates

---
+++ precache-manifest.e49c52d59e58774f5941b83fa1aaf62d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c9c085a1776314c1812c",
+    "url": "/hello/static/js/runtime~main.c9c085a1.js"
+  },
+  {
+    "revision": "d4b9c0fe60aa64325ee7",
+    "url": "/hello/static/js/main.d4b9c0fe.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/hello/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "d4b9c0fe60aa64325ee7",
+    "url": "/hello/static/css/main.f1b13089.chunk.css"
+  },
+  {
+    "revision": "db80fcaf756273a02b6126ffc0f59340",
+    "url": "/hello/index.html"
+  }
+];
