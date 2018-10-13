91fdddae730c02ee85c5827f3e2885ecd9edffc1
Ogbara G. Godstar <colossalboss@users.noreply.github.com>
2018-10-13 22:13:08 +0100

    Updates

---
+++ precache-manifest.355515d0fef62113fd7ced349be78d57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-begin/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aec13948c6c24a2b47e3",
+    "url": "/react-begin/static/js/runtime~main.aec13948.js"
+  },
+  {
+    "revision": "9bf725b32bac2a8fc52f",
+    "url": "/react-begin/static/js/main.9bf725b3.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/react-begin/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "9bf725b32bac2a8fc52f",
+    "url": "/react-begin/static/css/main.f1b13089.chunk.css"
+  },
+  {
+    "revision": "c0153de315082dcd50bc9e14efc4c9c8",
+    "url": "/react-begin/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.7bf799c5bcdbbed26acaadc102bb5ff2.js"
+  "/react-begin/precache-manifest.355515d0fef62113fd7ced349be78d57.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-app/index.html", {
+workbox.routing.registerNavigationRoute("/react-begin/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
